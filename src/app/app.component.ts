8e4b95e7b970f79ea834222e3ff04e0b1bcae6ab
Luis Abraham Tlapa Garcia <l.tlapa@ssaver.gob.mx>
2022-04-04 10:14:55 -0500

    FIX - Se resolvieron problemas de la tabla dinamica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { merge, Observable, of } from 'rxjs';
+
+class Todo {
+  id!: string;
+  description!: string;
+  complete!: boolean;
+  nombre!: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'TablaDinamica';
+
+  constructor() {
+    const todos: Todo[] = [
+      {
+        id: '123',
+        description: 'Complete me!',
+        nombre: 'Luis',
+        complete: false,
+      },
+    ];
+
+
+    this.dataSource = new MatTableDataSource(todos);
+    merge(
+      this.id!.valueChanges,
+      this.description!.valueChanges,
+      this.nombre!.valueChanges
+    ).subscribe(() => {
+      this.displayedColumns$ = of(
+        this.columnDefinitions.filter((c) => c.cb?.value).map((c) => c.def)
+      );
+    });
+  }
+
+  form: FormGroup = new FormGroup({
+    id: new FormControl(false),
+    description: new FormControl(false),
+    nombre: new FormControl(false),
+  });
+
+
+  id = this.form.get('id');
+  description = this.form.get('description');
+  nombre = this.form.get('nombre');
+
+  columnDefinitions = [
+    { def: 'id', label: 'ID', cb: this.id },
+    { def: 'description', label: 'Description', cb: this.description },
+    { def: 'nombre', label: 'nombre', cb: this.nombre },
+  ];
+
+  displayedColumns$: Observable<string[]> = of(
+    this.columnDefinitions.map((c) => c.def)
+  );
+
+  dataSource: MatTableDataSource<Todo>;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    FormsModule,
+    MatTableModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
